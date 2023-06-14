a45c97bf663b4a68fcf9541b4fa43310ad6eb800
SofiaSoengas <sofiasoengas1@gmail.com>
2023-06-13 22:34:22 -0300

    planes

---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPlanImpl.java
@@ -0,0 +1,60 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unlam.tallerweb1.modelo.Plan;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+
+@Repository
+public class RepositorioPlanImpl implements RepositorioPlan {
+	
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	public RepositorioPlanImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public void darDeBajaPlan(Long id) {
+		final Session session = sessionFactory.getCurrentSession();
+		Plan plan1 = (Plan) session.createCriteria(Plan.class).add(Restrictions.eq("id", id)).uniqueResult();
+		sessionFactory.getCurrentSession().delete(plan1);
+		
+	}
+
+	@Override
+	public List<Plan> listaDePlanes() {
+		final Session session = sessionFactory.getCurrentSession();
+
+		return session.createCriteria(Plan.class).list();
+	}
+
+	@Override
+	public Plan consultarPlan(Long id) {
+		final Session session = sessionFactory.getCurrentSession();
+		return (Plan) session.createCriteria(Plan.class).add(Restrictions.eq("id", id)).uniqueResult();
+	}
+
+	@Override
+	public void asignarPlanAUsuario(Usuario usuario, Plan plan) {
+		final Session session = sessionFactory.getCurrentSession();
+		Usuario u1= (Usuario) session.createCriteria(Usuario.class)
+				.add(Restrictions.eq("id",usuario.getId()))
+				.uniqueResult();
+		         u1.setPlan(plan);
+		         u1.setPuntos(plan.getPuntos());
+		         
+		         
+	}
+		
+	}
+
+
