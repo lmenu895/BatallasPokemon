566d5d98dbd72c61977f1c56f6b82011df0c4087
SofiaSoengas <sofiasoengas1@gmail.com>
2023-06-25 20:17:44 -0300

    Mercado Pago

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPlan.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPlan.java
@@ -1,6 +1,9 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,6 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
+import com.mercadopago.client.preference.PreferenceClient;
+import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.preference.Preference;
 
 import ar.edu.unlam.tallerweb1.exceptions.SaldoInsuficienteException;
 import ar.edu.unlam.tallerweb1.exceptions.UsuarioSinBilleteraException;
@@ -53,6 +65,8 @@ public class ControladorPlan {
 		Long idUsuario = (Long) request.getSession().getAttribute("id");
 		Usuario u1 = servicioUsuario.buscar(idUsuario);
 		Billetera billetera = servicioBilletera.consultarBilleteraDeUsuario(u1.getId());
+		
+		
 		if (u1 != null) {
 			if (servicioUsuarioPlan.buscarPlanPorUsuario(idUsuario) == null) {
 				if (billetera != null) {
@@ -72,14 +86,54 @@ public class ControladorPlan {
 		}
 		return new ModelAndView("redirect:/login");
 	}
+	
+	@RequestMapping(path = "/generarPago/{planId}", method = RequestMethod.GET)
+	public ModelAndView planes(@PathVariable Long planId, HttpServletRequest request) {
+		if (request.getSession().getAttribute("usuario") == null) {
+			return new ModelAndView("redirect:/login");
+		}
+		Plan plan = servicioPlan.consultarPlan(planId);
+		
+        MercadoPagoConfig.setAccessToken("TEST-4273752139461683-062512-21434a011fdba96b368ddb48a0a2f168-164214839");
+		PreferenceClient client = new PreferenceClient();
+		// Crea un ítem en la preferencia
+		PreferenceItemRequest item =
+		   PreferenceItemRequest.builder()
+		       .title(plan.getNombre())
+		       .quantity(1)
+		       .unitPrice(new BigDecimal(plan.getPrecio()))
+		       .build();
+		List<PreferenceItemRequest> items = new ArrayList<>();
+		items.add(item);
+		PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest
+                .builder()
+                .success("http://localhost:8080/batallas-pokemons/planAsignadoCorrectamente/")
+                .failure("http://localhost:8080/batallas-pokemons/planAsignadoCorrectamente/")
+                .pending("http://localhost:8080/batallas-pokemons/planAsignadoCorrectamente/")
+                .build();
+		PreferenceRequest prefRequest =
+		   PreferenceRequest.builder().items(items).backUrls(backUrls).build();
+        Preference response = null;
+		try {
+			response = client.create(prefRequest);
+		} catch (MPException | MPApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return new ModelAndView("redirect:" + response.getInitPoint());
+	}
 
 	@RequestMapping(path = "asignarplan/{plan}", method = RequestMethod.GET)
-	public ModelAndView elegirPlan(@PathVariable("plan") Long idP, HttpServletRequest request) {
+	public ModelAndView elegirPlan(@PathVariable("plan") Long idP, HttpServletRequest request) throws MPException, MPApiException {
 		if (request.getSession().getAttribute("usuario") == null) {
 			return new ModelAndView("redirect:/login");
 		}
 
 		ModelMap modelo = new ModelMap();
+		Plan plan = servicioPlan.consultarPlan(idP);
+	
+		
 		try {
 			this.servicioUsuarioPlan.asignarPlan(idP, (Long) request.getSession().getAttribute("id"));
 			return new ModelAndView("redirect:/planAsignadoCorrectamente");
